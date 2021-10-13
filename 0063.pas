6d3c2128e6c18ad67bbe7303f9ed40a1baf977a5
Matvey2009 <matvey2009ua@gmail.com>
2021-10-13 21:20:13 +0300

    F

---
+++ 0063.pas
@@ -0,0 +1,16 @@
+﻿program h;
+var X, Y, S, P: integer;
+
+begin
+  read(S, P);
+  
+  for X := 0 to S do
+  begin
+    Y := S - X;
+    if X * Y = P then
+       begin
+        writeln(X, ' ',  Y);
+        break;
+        end;
+  end;
+  end.
