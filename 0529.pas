3192d4ca2fa3dd6974ef6fa4dd4fa637ab964dc0
Matvey2009 <matvey2009ua@gmail.com>
2021-10-19 20:58:14 +0300

    Create 0529.pas

---
+++ 0529.pas
@@ -0,0 +1,13 @@
+﻿program p;
+var X1, Y1, X2, Y2, catX, catY: integer;
+S: real;
+
+begin
+  read(X1, Y1, X2, Y2);
+  
+  catX := X2 - X1;
+  catY := Y2 - Y1;
+  S := sqrt((catX * catX) + (catY * catY));
+  
+  writeln(S:0:5);
+end. 
