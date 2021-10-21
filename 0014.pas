8b5680a63561f4c438c8443bc9eee6cfb08f19fb
Matvey2009 <matvey2009ua@gmail.com>
2021-10-21 21:23:01 +0300

    Create 0014.pas

---
+++ 0014.pas
@@ -0,0 +1,15 @@
+﻿program p;
+var A, B, S, i: integer;
+
+begin
+  read(A, B);
+ 
+  for i := 1 to A * B do
+  begin
+    S := A * i;
+    if S mod B = 0 then
+      break;
+   
+    end;
+     writeln(S);
+end.
