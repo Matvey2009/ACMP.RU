25b2011e226193169249928125e5f12da2ac0335
Matvey2009 <matvey2009ua@gmail.com>
2022-01-16 12:27:11 +0300

    Create 0328.py

---
+++ 0328.py
@@ -0,0 +1,11 @@
+N = int(input())
+print(N * (N + 1) * (N + 2) // 2)
+
+
+
+#for i in range(N):
+#    for j in range(i,N):
+#        S+=i+j
+
+#for i in range(N + 1):
+#    S += i * (N - i + 1) + (i + N) * (N - i + 1) // 2
