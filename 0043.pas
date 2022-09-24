fcf08becf12e5770a29f94699744a470e67677ab
Matvey2009 <matvey2009ua@gmail.com>
2022-09-24 21:38:52 +0300

    Create 0043.pas

---
+++ 0043.pas
@@ -0,0 +1,21 @@
+﻿Program n0043;
+Var 
+       n:String;
+       count, temp:Integer;
+Begin
+  count := 0;
+  temp := 0;
+  readln(n);
+  For var i := 1 to length(n) do
+  Begin
+     if copy(n, i, 1) = '0' then
+     begin
+         temp := temp + 1;
+         if temp > count then
+           count := temp;
+     end
+     else
+           temp := 0;
+   End;
+   writeln(count);
+End.
