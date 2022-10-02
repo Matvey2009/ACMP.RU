58c177a7c4e3c9d9e1cddefa00632543a428c083
Matvey2009 <matvey2009ua@gmail.com>
2022-10-02 15:30:13 +0300

    Update 0691.pas

--- 0691.pas
+++ 0691.pas
@@ -1,3 +1,23 @@
 ﻿Program n0691;
-var
-  
+var n  : integer;
+       bn: string = 'ABCEHKMOPTXY';
+       bm:string = '1234567890';
+       s:    string;
+begin
+  readln(n);
+  for var i := 1 to n do
+  begin
+    readln(s);
+    if (length(s) = 6) and
+       (copy(s, 1,  1) in bn ) and
+       (copy(s, 2,  1) in bm) and
+       (copy(s, 3,  1) in bm) and
+       (copy(s, 4,  1) in bm) and
+       (copy(s, 5,  1) in bn ) and
+       (copy(s, 6,  1) in bn )
+   then
+     writeln('Yes')
+   else
+     writeln('No')
+  end;
+end.
