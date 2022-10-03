702fe12c668ad55dc39e7f26d3cff02993fed9a2
Matvey2009 <matvey2009ua@gmail.com>
2022-10-03 20:56:32 +0300

    Create 0147.pas

---
+++ 0147.pas
@@ -0,0 +1,15 @@
+﻿program p;
+var n, x,  x1, x2: integer;
+begin
+  readln(n);
+  x1 := 0;
+  x2 := 1;
+  for var i := 1 to n do
+  begin
+    x := x1+ x2;
+    x2 := x1;
+    x1 := x;
+  end;
+writeln(x);
+end.
+
