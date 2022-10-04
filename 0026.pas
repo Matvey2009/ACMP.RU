9cb5acd17b7503c9989f1d6e8d0455fda539dee9
Matvey2009 <matvey2009ua@gmail.com>
2022-10-04 20:49:02 +0300

    Create 0026.pas

---
+++ 0026.pas
@@ -0,0 +1,12 @@
+﻿program acmp0026;
+var x1, y1, r1, x2, y2, r2: integer;
+g: real;
+begin
+  readln(x1, y1, r1);
+  readln(x2, y2, r2);
+  g := sqrt(sqr(x1-x2) + sqr(y1-y2));
+  if (r1 + r2 >= g) and (abs(r1 - r2) <= g) then
+   writeln('YES')
+  else
+    writeln('NO')
+end.
