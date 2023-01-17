dc3ba690fcac3ee56eac9be20721c61b635cf8b9
Matvey2009 <matvey2009ua@gmail.com>
2023-01-17 20:47:59 +0300

    Create 0688.py

---
+++ 0688.py
@@ -0,0 +1,14 @@
+k=0
+x1, y1 = map(int, input().split())
+x2, y2 = map(int, input().split())
+n = int(input())
+for i in range(n):
+	x3, y3 = map(int, input().split())
+	sl = (abs(y1-y3)**2+abs(x1-x3)**2)**0.5
+	cl =((abs(y2-y3)**2+abs(x2-x3)**2)**0.5)/2
+	if (sl <= cl):
+		print(i+1)
+		k = 1
+		break
+if k != 1:		
+	print("NO")
