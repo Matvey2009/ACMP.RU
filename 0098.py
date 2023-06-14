d41ed0ce5e3df8dd1f9f98cc2e8b59cd960f7b88
Matvey2009 <matvey2009ua@gmail.com>
2023-06-14 18:46:28 +0300

    Update 0098.py

--- 0098.py
+++ 0098.py
@@ -1,19 +1,27 @@
 n = int(input())
 arr = list(map(int, input().split()))
-p = v = 0
+p = v = l = 0
+r = n-1
 for i in range(n):
-	print(i, arr[i], n - i - 1, arr[n - i - 1])
-	if(arr[i] >= arr[n - i - 1]):
-		if (i % 2 != 0):
-			p += arr[i]
+	#print(i, arr[i], n - i - 1, arr[n - i - 1])#
+	if(arr[l] >= arr[r]):
+		if (i % 2 == 0):
+			p += arr[l]
+			#print(arr[l], "Первый начало")
+			l += 1
 		else:
-			v += arr[i]
+			v += arr[l]
+			#print(arr[l], "Второй начало")
+			l += 1
 		#arr.pop(i)
 	else:
-		if (i % 2 != 0):
-			p += arr[n - i - 1]
+		if (i % 2 == 0):
+			p += arr[r]
+			#print(arr[r], "Первый последний")
+			r -= 1
 		else:
-			v += arr[n - i - 1]
-		p += arr[n - i - 1]
+			v += arr[r]
+			#print(arr[r], "Второй последний")
+			r -= 1
 		#arr.pop(n - i - 1)
-print(p,":",v)
+print(str(p)+":"+str(v))
