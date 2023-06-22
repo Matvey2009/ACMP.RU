0550b8e25f07edc219b32f74f50fa72609fd5a90
Matvey2009 <matvey2009ua@gmail.com>
2023-06-22 21:12:01 +0300

    Update 0499.py

--- 0499.py
+++ 0499.py
@@ -2,6 +2,12 @@ ch, m = map(int, input().split())
 a1, b1, a2, b2, a3, b3 = map(int, input().split())
 k1, k2, k3 = b1/a1, b2/a2, b3/a3
 
-if(max(k1, k2, k3) == k1):
-	if(a1 < m):
-		s += 
+if(max(k1, k2, k3) == k1 and a1 < m):
+	ch -= b1
+	m -= a1
+elif(max(k1, k2, k3) == k2 and a2 < m):
+	ch -= b1
+	m -= a1
+elif(max(k1, k2, k3) == k3 and a3 < m):
+	ch -= b1
+	m -= a1
