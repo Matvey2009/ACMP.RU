ba8f590c00c620180dfc1c8f401ce0501565ad70
Matvey2009 <matvey2009ua@gmail.com>
2023-06-28 19:36:39 +0300

    Update 0693.py

--- 0693.py
+++ 0693.py
@@ -1,8 +1,17 @@
-st = input()
-x = int(input())
-if(x >= 2 and st == "Home"):
-	print("No")
-elif(x >= 1 and st == "School"):
+arra = []
+arrb = []
+a, b = input().split()
+if (len(a) != len(b)):
 	print("No")
 else:
-	print("Yes")
+	for i in a.lower():
+		arra.append(i)
+	for i in b.lower():
+		arrb.append(i)
+	arra.sort()
+	arrb.sort()
+	if(arra == arrb):
+		print("Yes")
+	else:
+		print("No")
+
