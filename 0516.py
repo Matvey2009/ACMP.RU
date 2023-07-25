86978cc72cfd6e15391629627437a5c41b5374a3
Matvey2009 <matvey2009ua@gmail.com>
2023-07-25 23:37:39 +0300

    Update 0516.py

--- 0516.py
+++ 0516.py
@@ -1,15 +1,28 @@
 n = input()
-k = n[::-1]
-t1 = t2 = 0 
-for i in range(1, int(n)):
-	if(int(n)%i == 0):
-		t1+=1
-for i in range(1, int(k)):
-	if(int(k)%i == 0):
-		t2+=1
-		
-if(t1>=0 or t2>=0):
-	print("No")
+arr=[] 	
+arrr=[]
+for i in n:
+	arr.append(i)
+	arrr.append(i)
+if "0" in arr:
+	print("No", 1)
 else:
-	print("Yes")
+	arr.sort()
+	arrr.sort()
+	arrr.reverse()
+	r = ''.join(arr)
+	e = ''.join(arrr)
+	t1 = t2 = 0 
+	for i in range(2, int(e)):
+		if(int(e)%i == 0):
+			t2+=1
+			break
+	for i in range(2, int(r)):
+		if(int(r)%i == 0):
+			t1+=1
+			break
+	if t1== 0 and t2==0:
+		print("Yes")
+	else:
+		print("No")
 
